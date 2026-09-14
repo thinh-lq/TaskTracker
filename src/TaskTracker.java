223f2d50581d14ec00c0566712b0110e8211de97
thinhlq <lequangthinhbe@gmail.com>
2026-09-14 16:42:19 +0700

    Update gitignore

--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -1,4 +1,23 @@
-void main() {
-    System.out.println(233);
-    System.out.println(348387835);
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
+public class TaskTracker {
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("task.json");
+
+        String json = """
+                [
+                    {
+                        "id": 1,
+                        "status": "todo"
+                    }
+                    {
+                        "id": 2,
+                        "status": "done"
+                    }
+                ]
+                """;
+        Files.writeString(path, json);
+    }
 }
