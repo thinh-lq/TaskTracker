e52960a52f74dc3833ea8049b896348416018479
thinhlq <lequangthinhbe@gmail.com>
2026-09-16 21:12:23 +0700

    Add time

--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
+import java.time.LocalDateTime;
 
 class Helper {
     static void printString() throws IOException {
@@ -13,18 +14,23 @@ class Helper {
 
         int start = line.indexOf("\"id\": ") + "\"id\": ".length();
         int end = line.indexOf(", \"description\"", start);
-
         task.setId(Integer.parseInt(line.substring(start, end)));
 
-        start = line.indexOf("\"description\": ") + "\"description\": ".length()+1;
-        end = line.indexOf(", \"status\"", start)-1;
-
+        start = line.indexOf("\"description\": ") + "\"description\": ".length() + 1;
+        end = line.indexOf(", \"status\"", start) - 1;
         task.setDescription(line.substring(start, end));
 
-        start = line.indexOf("\"status\": ") + "\"status\": ".length()+1;
-        end = line.length()-1;
-
+        start = line.indexOf("\"status\": ") + "\"status\": ".length() + 1;
+        end = line.indexOf(", \"createdAt\"", start) - 1;
         task.setStatus(line.substring(start, end));
+
+        start = line.indexOf("\"createdAt\": ") + "\"createdAt\": ".length() + 1;
+        end = line.indexOf(", \"updatedAt\"", start) - 1;
+        task.setCreatedAt(LocalDateTime.parse(line.substring(start, end)));
+
+        start = line.indexOf("\"updatedAt\": ") + "\"updatedAt\": ".length() + 1;
+        end = line.length() - 1;
+        task.setUpdatedAt(LocalDateTime.parse(line.substring(start, end)));
 
         return task;
     }
@@ -33,6 +39,8 @@ class Helper {
         return "{\"id\": " + task.getId()
                 + ", \"description\": \"" + task.getDescription()
                 + "\", \"status\": \"" + task.getStatus()
+                + "\", \"createdAt\": \"" + task.getCreatedAt()
+                + "\", \"updatedAt\": \"" + task.getUpdatedAt()
                 + "\"}";
     }
 
@@ -58,7 +66,13 @@ class Helper {
     static void add(String description) throws IOException {
         List<Task> tasks = readData();
 
-        Task task = new Task(description);
+        int maxId = 0;
+
+        for (Task task : tasks) {
+            maxId = Math.max(maxId, task.getId());
+        }
+
+        Task task = new Task(maxId + 1, description);
         tasks.add(task);
 
         StringBuilder str = new StringBuilder("[\n");
@@ -85,21 +99,26 @@ class Helper {
 }
 
 class Task {
-    private static int cnt;
     private int id;
     private String description;
     private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public Task() {
         id = 0;
         description = "";
         status = "";
+        createdAt = null;
+        updatedAt = null;
     }
 
-    public Task(String description) {
-        id = ++cnt;
+    public Task(int id, String description) {
+        this.id = id;
         this.description = description;
-        status = "todo";
+        this.status = "todo";
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
     }
 
     public int getId() {
@@ -126,13 +145,30 @@ class Task {
         this.status = status;
     }
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
     @Override
     public String toString() {
         return "Id: " + id
-             + ", Description: " + description
-             + ", Status: " + status;
+                + ", Description: " + description
+                + ", Status: " + status
+                + ", CreatedAt: " + createdAt
+                + ", UpdatedAt: " + updatedAt;
     }
-
 }
 
 public class TaskTracker {
